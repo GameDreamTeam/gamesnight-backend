493b4d5ec2aae2cf4f5d34df8e39c314d97c6f27
Ayush <lionelayush446@gmail.com>
2024-01-23 04:34:52 +0530

    prod redis

--- internal/database/redis.go
+++ internal/database/redis.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/alicebob/miniredis"
 	"github.com/go-redis/redis"
+	"os"
 )
 
 type RedisClient struct {
@@ -28,16 +29,28 @@ func getLocalClient() (*redis.Client, error) {
 }
 
 func NewRedisClient() {
+	var rdb *redis.Client
+    var err error
 
 	if config.Get().Env == "local" {
-		rdb, err := getLocalClient()
+		rdb, err = getLocalClient()
 
-		if err != nil {
-			panic(fmt.Sprintf("Redis Initialization failed for env%s with error %s", config.Get().Env, err))
-		}
+	}else{
+		redisAddr := os.Getenv("REDIS_ADDR") // e.g., "prod-redis-host:6379"
+        redisPassword := os.Getenv("REDIS_PASSWORD") // e.g., "mysecretpassword"
 
-		rc = &RedisClient{
-			Client: rdb,
-		}
+        rdb = redis.NewClient(&redis.Options{
+            Addr:     redisAddr,
+            Password: redisPassword, // no password set for empty string
+            DB:       0, // default DB
+        })
+	}
+	
+	if err != nil {
+		panic(fmt.Sprintf("Redis Initialization failed for env%s with error %s", config.Get().Env, err))
+	}
+
+	rc = &RedisClient{
+		Client: rdb,
 	}
 }
